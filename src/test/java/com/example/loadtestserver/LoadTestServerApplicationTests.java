2c490ec70f8943f76816d7f717286ae878bb9f5c
Alex Menikoff <alexmenikoff@gmail.com>
2024-09-10 16:25:18 -0400

    Made AlexBranch and attempted to add to the test file

--- src/test/java/com/example/loadtestserver/LoadTestServerApplicationTests.java
+++ src/test/java/com/example/loadtestserver/LoadTestServerApplicationTests.java
@@ -2,12 +2,33 @@ package com.example.loadtestserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.junit.jupiter.api.Assertions.*;
+
+
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class LoadTestServerApplicationTests {
+  
+  @Autowired
+    private MockMvc mockMvc;
 
   @Test
-  void contextLoads() {
+  public void testStatusCode() throws Exception {
+    
+    //need more setup
+    mockMvc.perform(get("/.....")).andExpect(status().isOk());
   }
 
 }
